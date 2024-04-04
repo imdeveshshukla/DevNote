5f4938d1acac2e9f2b44d5598f97c48da3537e49
Devesh Shukla <deveshshukla1603@gmail.com>
2024-04-04 23:50:30 +0530

    frontendcomponents

--- devBlog/src/Components/Input.tsx
+++ devBlog/src/Components/Input.tsx
@@ -1,5 +1,31 @@
-export function Input({labelfor , hint}){
-    return<>
-        
-    </>
+import { ChangeEvent } from "react";
+
+
+type param = {
+    label:string,
+    placeholder:string,
+    value: string,
+    onChange: (value: string) => void
 }
+export function Input({ label, placeholder, value, onChange }:param){
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+      onChange(e.target.value); 
+    };
+  
+    return (
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" >
+          {label}
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="text"
+          placeholder={placeholder}
+          value={value}
+          onChange={handleChange}
+        />
+      </div>
+    );
+  };
+  
+  export default Input;
--- devBlog/src/pages/Signin.tsx
+++ devBlog/src/pages/Signin.tsx
@@ -1,5 +1,22 @@
+import { ChangeEvent, useState } from "react";
+import { Input } from "../Components/Input";
+
 export function Signin(){
-    return<>
-    
-    </>
+    const [username,setUsername] = useState("");
+    const [pass,setPass] = useState("");
+    const handleUsername = (value: string) => {
+        setUsername(value);
+      };
+      const handlePass = (value: string) => {
+        setPass(value);
+      };
+    return<div className="flex justify-center w-full h-screen">
+
+        <div className="self-center">
+            <Input label="Username" placeholder="Enter your Username" value={username} onChange={handleUsername}/>
+            <Input label="Password" placeholder="Enter your Password" value={pass} onChange={handlePass}/>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={handlePass}>Sign In</button>
+        </div>
+
+    </div>
 }
